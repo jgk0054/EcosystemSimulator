0d3d31837bbd66e2ecf5566ab3baa68576d74843
John <johngknowles77@gmail.com>
2019-10-15 22:05:24 -0500

    Added object spawning and some new materials

--- Scripts/Camera_Script.cs
+++ Scripts/Camera_Script.cs
@@ -1,69 +1,146 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
 
 public class Camera_Script : MonoBehaviour
 {
 
-    /*
-	EXTENDED FLYCAM
-		Desi Quintans (CowfaceGames.com), 17 August 2012.
-		Based on FlyThrough.js by Slin (http://wiki.unity3d.com/index.php/FlyThrough), 17 May 2011.
- 
-	LICENSE
-		Free as in speech, and free as in beer.
- 
-	FEATURES
-		WASD/Arrows:    Movement
-		          Q:    Climb
-		          E:    Drop
-                      Shift:    Move faster
-                    Control:    Move slower
-                        End:    Toggle cursor locking to screen (you can also press Ctrl+P to toggle play mode on and off).
-	*/
+    public float ScreenEdgeBorderThickness = 5.0f; // distance from screen edge. Used for mouse movement
 
-    public float cameraSensitivity = 90;
-    public float climbSpeed = 4;
-    public float normalMoveSpeed = 10;
-    public float slowMoveFactor = 0.25f;
-    public float fastMoveFactor = 3;
 
-    private float rotationX = 0.0f;
-    private float rotationY = 0.0f;
+    [Header("Movement Speeds")]
+    [Space]
+    public float minPanSpeed;
+    public float maxPanSpeed;
+    public float secToMaxSpeed; //seconds taken to reach max speed;
+    public float zoomSpeed;
 
+    [Header("Movement Limits")]
+    [Space]
+    public bool enableMovementLimits;
+    public Vector2 heightLimit;
+    public Vector2 lenghtLimit;
+    public Vector2 widthLimit;
+    private Vector2 zoomLimit;
+
+    private float panSpeed;
+    private Vector3 initialPos;
+    private Vector3 panMovement;
+    private Vector3 pos;
+    private Quaternion rot;
+    private bool rotationActive = false;
+    private Vector3 lastMousePosition;
+    private Quaternion initialRot;
+    private float panIncrease = 0.0f;
+
+    [Header("Rotation")]
+    [Space]
+    public float rotateSpeed;
+
+
+
+
+
+    // Use this for initialization
     void Start()
     {
+        initialPos = transform.position;
+        initialRot = transform.rotation;
+        zoomLimit.x = 15;
+        zoomLimit.y = 65;
     }
+
 
     void Update()
     {
-        rotationX += Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
-        rotationY += Input.GetAxis("Mouse Y") * cameraSensitivity * Time.deltaTime;
-        rotationY = Mathf.Clamp(rotationY, -90, 90);
 
-        transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
-        transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
 
-        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+
+        panMovement = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
         {
-            transform.position += transform.forward * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
-            transform.position += transform.right * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
+            panMovement += Vector3.forward * panSpeed * Time.deltaTime;
         }
-        else if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+        if (Input.GetKey(KeyCode.S))
         {
-            transform.position += transform.forward * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
-            transform.position += transform.right * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
+            panMovement -= Vector3.forward * panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            panMovement += Vector3.left * panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            panMovement += Vector3.right * panSpeed * Time.deltaTime;
+            //pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            panMovement += Vector3.up * panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            panMovement += Vector3.down * panSpeed * Time.deltaTime;
+        }
+
+        transform.Translate(panMovement, Space.Self);
+
+
+        //increase pan speed
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)
+            || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)
+            || Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.Q)
+            || Input.mousePosition.y >= Screen.height
+            || Input.mousePosition.x >= Screen.width)
+        {
+            panIncrease += Time.deltaTime / secToMaxSpeed;
+            panSpeed = Mathf.Lerp(minPanSpeed, maxPanSpeed, panIncrease);
         }
         else
         {
-            transform.position += transform.forward * normalMoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
-            transform.position += transform.right * normalMoveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+            panIncrease = 0;
+            panSpeed = minPanSpeed;
         }
 
 
-        if (Input.GetKey(KeyCode.Q)) { transform.position += transform.up * climbSpeed * Time.deltaTime; }
-        if (Input.GetKey(KeyCode.E)) { transform.position -= transform.up * climbSpeed * Time.deltaTime; }
+        // Mouse Rotation
+        if (Input.GetMouseButton(2))
+        {
+            rotationActive = true;
+            Vector3 mouseDelta;
+            if (lastMousePosition.x >= 0 &&
+                lastMousePosition.y >= 0 &&
+                lastMousePosition.x <= Screen.width &&
+                lastMousePosition.y <= Screen.height)
+                mouseDelta = Input.mousePosition - lastMousePosition;
+            else
+            {
+                mouseDelta = Vector3.zero;
+            }
+            var rotation = Vector3.up * Time.deltaTime * rotateSpeed * mouseDelta.x;
+            rotation += Vector3.left * Time.deltaTime * rotateSpeed * mouseDelta.y;
+
+            transform.Rotate(rotation, Space.World);
+
+            // Make sure z rotation stays locked
+            rotation = transform.rotation.eulerAngles;
+            rotation.z = 0;
+            transform.rotation = Quaternion.Euler(rotation);
+        }
+
+        if (Input.GetMouseButtonUp(2))
+        {
+            rotationActive = false;
+        }
+
+        lastMousePosition = Input.mousePosition;
+
 
 
     }
+
 }
+
+
+
---
+++ Scripts/UI_Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Script : MonoBehaviour
+{
+    [Header("List of spawnables")]
+    public GameObject bush;
+    public GameObject tree;
+
+    [Header("Spawner Script Ref")]
+    public GenerateObject objectGen;
+
+    public void selectTree()
+    {
+        objectGen.prefab = tree;
+    }
+
+    public void selectBush()
+    {
+        objectGen.prefab = bush;
+    }
+}
